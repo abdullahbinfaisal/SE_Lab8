496fd12e846369d817cb7c6e70092ff2f6cb6d15
Abdullah Bin Faisal <paybacktanki@gmail.com>
2026-03-19 16:10:49 +0500

    has city should fail

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,7 +1,31 @@
 package com.example.lab08;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomListTest {
 
+    private CustomList list;
+    private City calgary;
+    private City edmonton;
+
+    @BeforeEach
+    public void setUp() {
+        list = new CustomList();
+        calgary = new City("Calgary", "AB");
+        edmonton = new City("Edmonton", "AB");
+    }
+
+    @Test
+    public void testHasCityReturnsTrueWhenCityExists() {
+        list.addCity(calgary);
+        assertTrue(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testHasCityReturnsFalseWhenCityAbsent() {
+        list.addCity(calgary);
+        assertFalse(list.hasCity(edmonton));
+    }
 }
